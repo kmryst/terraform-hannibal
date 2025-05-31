42f5ed70de1d340f5a48117e508e76ba77bd639e
gatsby <gatsbykenji@gmail.com>
2025-05-31 16:54:07 +0900

    deploy.yml修正7

--- terraform/frontend/outputs.tf
+++ terraform/frontend/outputs.tf
@@ -8,3 +8,8 @@ output "s3_bucket_name" {
   description = "Name of the S3 bucket for frontend files"
   value       = aws_s3_bucket.frontend_bucket.bucket
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.main.id
+}
