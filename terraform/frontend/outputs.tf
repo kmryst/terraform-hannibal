d5b7c974f7cbaf6c23938db41f6407b978511058
gatsby <gatsbykenji@gmail.com>
2025-06-28 19:23:02 +0900

    Terraformなど更新33

--- terraform/frontend/outputs.tf
+++ terraform/frontend/outputs.tf
@@ -6,7 +6,7 @@ output "cloudfront_domain_name" {
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket for frontend files"
-  value       = aws_s3_bucket.frontend_bucket.bucket
+  value       = data.aws_s3_bucket.frontend_bucket.bucket
 }
 
 output "cloudfront_distribution_id" {
