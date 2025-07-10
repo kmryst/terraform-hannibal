800352d61dbb574b4aa09b2b25b59cc10eb8f822
kmryst <viculusfelis@gmail.com>
2025-07-10 17:04:28 +0900

    PostgreSQL修正3

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -119,7 +119,7 @@ variable "db_allocated_storage" {
 variable "db_engine_version" {
   description = "PostgreSQL engine version"
   type        = string
-  default     = "15.4"
+  default     = "15.8"
 }
 
 variable "db_name" {
